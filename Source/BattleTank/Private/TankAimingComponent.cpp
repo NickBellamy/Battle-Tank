55efc12234fa3d96616bf67803af82b42a70fb05
Nick Bellamy <nick.bellamy@gmail.com>
2017-10-13 11:19:17 +0100

    Add missing parameter in function call
    There's some confusion as to whether there is/was a bug in the way that SuggestProjectileVelocity() works (https://answers.unrealengine.com/questions/435995/suggestprojectilevelocity-behaving-badly.html), so I've asked for clarification in a forum, so I can determine for myself which TraceOption should be used in the function call.

--- Source/BattleTank/Private/TankAimingComponent.cpp
+++ Source/BattleTank/Private/TankAimingComponent.cpp
@@ -35,8 +35,12 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	// Calculate the Launch Velocity and convert to unit vector if successful
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
-	if (UGameplayStatics::SuggestProjectileVelocity(this, OutLaunchVelocity, StartLocation, HitLocation,
-													LaunchSpeed, ESuggestProjVelocityTraceOption::DoNotTrace))
+	if (UGameplayStatics::SuggestProjectileVelocity(this, OutLaunchVelocity, StartLocation, 
+													HitLocation, LaunchSpeed, false, 0.0f, 0.0f,
+													// TODO Is DoNotTrace the correct parameter?
+													// Asked for clarification here:
+													// https://community.gamedev.tv/t/whats-the-difference-between-the-parameter-types/45040
+													ESuggestProjVelocityTraceOption::DoNotTrace))
 	{
 		// Convert to unit vector to get direction of launch
 		FVector AimDirection = OutLaunchVelocity.GetSafeNormal();
