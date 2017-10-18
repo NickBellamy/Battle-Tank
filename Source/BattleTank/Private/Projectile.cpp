173c03287d4ccd11215434448dcb824f2715a966
Nick Bellamy <nick.bellamy@gmail.com>
2017-10-18 11:11:11 +0100

    Attach ProjectileMovementComponent to the Projectile class
    This is done programmatically, rather than via Blueprint.  This is beneficial because the ProjectileMovementComponent will be an integral part of the class, so preventing it being accidentally removed from the Projectile blueprint is a good idea!

--- Source/BattleTank/Private/Projectile.cpp
+++ Source/BattleTank/Private/Projectile.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 
 // Sets default values
@@ -8,6 +9,12 @@ AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// No need to protect pointer as it's added at construction
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Porjectile Movement"));
+
+	// Prevent projectile firing on initialization
+	ProjectileMovement->bAutoActivate = false;
 
 }
 
