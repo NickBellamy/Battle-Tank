c90b4b49ac7a5f466e3182c3281fd05c82eb9402
Nick Bellamy <nick.bellamy@gmail.com>
2017-10-23 13:53:50 +0100

    Remove ability to drive the tank through the gamepad shoulder buttons
    This proved beneficial as a learning experience, but it feels a bit clunky.  Removing the bindings from the game solves all potential "double speed" issues that could arise from the duality of the controls.

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -29,12 +29,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	// Pointer protection
 	if (!LeftTrack || !RightTrack) { return; }
 
-	// TODO Clamp the Throw value between +1 and -0.5
-	
-	// TODO Prevent double speed due to dual control setup
-	// Pushing forward on the left stick and holding both shoulder buttons down simultaneously
-	// makes the tank go twice as fast as it should
-
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 
@@ -44,8 +38,6 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 {
 	// Pointer protection
 	if (!LeftTrack || !RightTrack) { return; }
-
-	// TODO Prevent double speed due to dual control setup
 
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
