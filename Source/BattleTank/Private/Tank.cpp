f868479349caf5a5bb8c96cfb0af0a8102a89c37
Nick Bellamy <nick.bellamy@gmail.com>
2017-11-03 10:04:08 +0000

    Added health bars to Tanks

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -27,3 +27,8 @@ float ATank::TakeDamage(float DamageAmount,	struct FDamageEvent const & DamageEv
 	return DamageToApply;
 
 }
+
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;	// Cast as floats because values are of type int32
+}
--- Source/BattleTank/Public/Tank.h
+++ Source/BattleTank/Public/Tank.h
@@ -18,9 +18,12 @@ public:
 	// Called by the engine when AActor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	// Returns current health as a value between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")		// BlueprintPure is essentially "const" for Blueprints
+		float GetHealthPercent() const;
+
 private:
-	// Gives an explicit variable name in Blueprint that can be adjusted by the designer
-	// rather than relying on them to change the value with the ambiguous name of "CurrentHealth"
+	// Used in calculation of GetHealthPercent()
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 		int32 StartingHealth = 100;
 
